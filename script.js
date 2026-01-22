949e2c3d045e8934d10bf6a73dcda67e00e22f4d
mille-zarcenis <jamillezarcenis@gmail.com>
2026-01-22 11:11:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const paginas = document.querySelectorAll(".pagina");
+
+const ultimaPagina = paginas[paginas.length - 1];
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // quando a última imagem aparece
+        setTimeout(() => {
+          window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+          });
+        }, 800);
+      }
+    });
+  },
+  {
+    threshold: 0.6
+  }
+);
+
+observer.observe(ultimaPagina);
 
